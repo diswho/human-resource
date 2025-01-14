810cd47c8624e7caba4d4e1e1b845a201bb0c9ba
diswho <discover.who@gmail.com>
2025-01-14 13:49:48 +0700

    02

--- backend/app/models/hr_salaries.py
+++ backend/app/models/hr_salaries.py
@@ -4,11 +4,10 @@ from decimal import Decimal
 
 
 class HRSalaryBase(SQLModel):
-    salary_date: date | None = Field(default=None)
-    base_salary: Decimal | None = Field(default=None)
-    fuel_allowance: Decimal | None = Field(default=None)
-    risk_allowance: Decimal | None = Field(default=None)
-
+    salary_date: date 
+    base_salary: Decimal 
+    fuel_allowance: Decimal 
+    risk_allowance: Decimal 
 
 class HRSalary(HRSalaryBase, table=True):
     __tablename__ = 'hr_salaries'
@@ -16,6 +15,9 @@ class HRSalary(HRSalaryBase, table=True):
     employee_id: int | None = Field(default=None, foreign_key='hr_employee.id')
 
 
-class HRSalaryUpdate(HRSalary):
-    # id: int | None =  Field(default=None)
-    pass
+class HRSalaryUpdate(HRSalaryBase):
+    employee_id: int 
+    salary_date: date | None = Field(default=None)
+    base_salary: Decimal | None = Field(default=None)
+    fuel_allowance: Decimal | None = Field(default=None)
+    risk_allowance: Decimal | None = Field(default=None)
